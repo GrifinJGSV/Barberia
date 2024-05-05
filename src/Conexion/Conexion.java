9f71a0f08b35682cdca67b1c470199f2e654fa16
HarolMendoza <arturofonseca187@gmail.com>
2024-05-04 20:13:30 -0600

    Arturo
    Correcion de los errores del checkList.

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -8,28 +8,45 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/*
+    * Nombre del archivo: Conexion.java
+    * Autor: Arturo Mendoza
+    * Fecha de creación: [20/09/2023]
+    * Descripción: Esta clase permite la conexion del sistema a la DB.
+    *              Permite a los desarrolladores establer una conexion con la DB cuando necesiten hacer la.
+    * Derechos de autor (c) [20/09/2023] Arturo Mendoza. Todos los derechos reservados.
+ */
+
 /**
  *
- * @author PC
+ * @author Arturo
  */
+    //Clase publica para para hacer la conexion a la base de datos en MySQL Workbench
     public class Conexion {
-
+        
+    //Metodo para obtener la conxion.
     public static Connection getConection() {
+        //regresa la conexion
         return conection;
     }
-
+    //Metodo para establecer la conexion
     public static void setConection(Connection conection) {
+        //variable que almacena la clase conection.
         Conexion.conection = conection;
     }
+    //metodo que inicializa la conexion en null
       public static Connection conection = null;
+    //meto que obtiene la conexion
     public Connection getConexion() {
+                //metodo para manejar las excepciones por si se efectua o falla la conexion.
 		try {
 			
-		//Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                      //Variable que utiliza una libreria para iniciar la conexion con los datos especificos de la conexion a la DB.
                       conection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/barberia", "root", "12345678");
                       return conection;
                         
                     } 
+                        //excepcion por si falla la conexion.
                        catch (SQLException ex) {
 			return conection;
 			
